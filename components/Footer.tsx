5ce176615b8970a8be26a367a0002f188fe3bdf2
ruphin314-eng <ruphinruphin314@gmail.com>
2026-05-14 12:11:15 +0100

    mise a jour

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -122,7 +122,7 @@ export default function Footer() {
             </div>
 
             <div className="text-sm text-white/60 space-y-1 text-center md:text-left">
-              <p>📧 <a href="mailto:contact@safyrr.tech" className="hover:underline">
+              <p>📧 <a href="mailto:support@safyrr.tech" className="hover:underline">
                      contact@safyrr.tech
                      </a>
               </p>
